d67d8d1239bc81ca3b6f0ee14818123934e88097
Sukhman Sidhu <sidhu.sk@obmontessori.edu.ph>
2026-03-17 12:40:04 +0800

    Fix rank thresholds and add invalid score alert

--- Activity 2.js
+++ Activity 2.js
@@ -2,16 +2,19 @@
 function compute_rank () {
     let math_score = Number(document.getElementById('math_score').value);
 
-    if(math_score > 94) {
+    if(math_score > 95) {
         window.alert('Gold')
     }
-    else if(math_score > 90-94) {
+    else if(math_score > 94) {
         window.alert('Silver')
     }
-    else if(math_score > 85-89) {
+    else if(math_score > 89) {
         window.alert('Bronze')
     }
-    else if(math_score > 80-84) {
+    else if(math_score > 84) {
         window.alert('Merit')
     }
+   else if (math_score > other) {
+    window.alert("Invalid");
+}
 }
